3d5511980178d39cfff55f1e415ffeef7d18ed96
Alec Rivers <alec@shapertools.com>
2023-06-09 08:25:58 -0700

    Overhaul JS to support iOS
    Also removes transitions for the moment

--- public/script.js
+++ public/script.js
@@ -1,9 +1,12 @@
+
+// ### CONSTANTS & GLOBALS ###
+
 var stations = [
   {
     // b&c, mellow
     "stream": 'https://eu10.fastcast4u.com:2650/stream/1/',
     "icon": "noun-vinyl-4463482.svg",
-    "audio": "turntable.mp3",
+    "name_audio": "turntable.mp3",
     "background-color": "#bfef45",		// lime
     "icon-color": "#3b6e00" // dark lime
   },
@@ -11,7 +14,7 @@ var stations = [
     // b&c, strong	robot
     "stream": 'https://streams.radiomast.io:443/0cef93cd-5974-43b1-868e-c739e81f4f2b',
     "icon": "noun-robot-1004193.svg",
-    "audio": "robot.mp3",
+    "name_audio": "robot.mp3",
     "background-color": "red",
     "icon-color": "#ff9999" // light red
   },
@@ -19,7 +22,7 @@ var stations = [
     // Frome TuneIn. Pop Hits.	star with music
     "stream": 'https://rfcmedia.streamguys1.com/newpophits.mp3',
     "icon": "noun-stars-1171664.svg",
-    "audio": "stars.mp3",
+    "name_audio": "stars.mp3",
     "background-color": "#12748a",		// cyan
     "icon-color": "#42d4f4" // dark cyan
   },
@@ -27,7 +30,7 @@ var stations = [
     // funk	disco ball
     "stream": 'https://radio4.cdm-radio.com:18008/stream-mp3-Funk',
     "icon": "noun-disco-5301484.svg",
-    "audio": "mirror_ball.mp3",
+    "name_audio": "mirror_ball.mp3",
     "background-color": "#f04a00",		// orange
     "icon-color": "#ff8c33" // light orange
   },
@@ -35,7 +38,7 @@ var stations = [
     // classic hits	cassette
     "stream": 'https://tunein-music.streamguys1.com/JACKFM-direct',
     "icon": "noun-cassette-1134380.svg",
-    "audio": "cassette.mp3",
+    "name_audio": "cassette.mp3",
     "background-color": "#fffac8",		// yellow / beige
     "icon-color": "#7f6b00" // dark yellow
   },
@@ -43,7 +46,7 @@ var stations = [
     // 80s	rubik's cube
     "stream": 'https://ors.cdnstream1.com/5214_128',
     "icon": "noun-rubiks-cube-571725.svg",
-    "audio": "rubiks_cube.mp3",
+    "name_audio": "rubiks_cube.mp3",
     "background-color": "#800000",		// maroon
     "icon-color": "#ff8080" // light maroon
   },
@@ -51,7 +54,7 @@ var stations = [
     // classic hip hop		boom box
     "stream": 'https://listen.181fm.com/181-oldschool_128k.mp3',
     "icon": "noun-boom-box-4888853.svg",
-    "audio": "boombox.mp3",
+    "name_audio": "boombox.mp3",
     "background-color": "#000075",		// navy
     "icon-color": "#7373ff" // light navy
   },
@@ -59,7 +62,7 @@ var stations = [
     // modern spanish	tequila
     "stream": 'https://strm112.1.fm/latino_mobile_mp3',
     "icon": "noun-tequila-4764344.svg",
-    "audio": "tequila.mp3",
+    "name_audio": "tequila.mp3",
     "background-color": "#911eb4",		// purple
     "icon-color": "#d69cf0" // light purple
   },
@@ -67,7 +70,7 @@ var stations = [
     // heavy metal	pirate flag
     "stream": 'https://s6-webradio.rockantenne.de/heavy-metal/stream/mp3',
     "icon": "noun-pirate-flag-787807.svg",
-    "audio": "jolly_roger.mp3",
+    "name_audio": "jolly_roger.mp3",
     "background-color": "#000000",		// black
     "icon-color": "#ffffff" // white
   },
@@ -75,53 +78,262 @@ var stations = [
     // electroswing	gramophone
     "stream": 'https://jazzradio.ice.infomaniak.ch/jazz-wr04-128.mp3',
     "icon": "noun-gramophone-3243255.svg",
-    "audio": "gramophone.mp3",
+    "name_audio": "gramophone.mp3",
     "background-color": "#f032e6",		// pink
     "icon-color": "#b30086" // dark pink
   }
 ];
-
-  /*
-  {
-    // "crazy taxi"-ish	taxi
-    "stream": 'https://mp3channels.webradio.antenne.de/punkrock',
-    "icon": "noun-taxi-10379.svg",
-    "audio": "taxi.mp3",
-    "background-color": "#a9a9a9",		// gray
-    "icon-color": "#4d4d4d" // dark gray
-  },
-  */
-  
-// Done
-
-var currentChannel = undefined; // undefined indicates intro page
 var numChannels = stations.length;
-var initialized = false;
-var fadeInterval = 20;    // In ms
-let introFade = 1.0;
-var fadeTime = 0.85; // In seconds
-let fadeStep = (fadeInterval / 1000.0) / fadeTime;
-console.log("Fade step: " + fadeStep);
+var audioContext = new (window.AudioContext || window.webkitAudioContext)();
+var audioBuffers = {};
+var logToPage = false;
 
-let audio = new Audio();
+// ### HELPER FUNCTIONS ###
 
-function startDisco() {
-    // Hide the landing page content
-    //document.getElementById('landing-page').style.display = 'none';
+function logText(text) {
+    if(logToPage) {
+        var textElement = document.createElement('p');
+        textElement.innerHTML = text;
+        document.getElementById("text-container").appendChild(textElement);
+    }
+    console.log(text);
+}
+
+function logError(text) {
+    if(logToPage) {
+        var textElement = document.createElement('p');
+        textElement.innerHTML = "ERROR: " + text;
+        document.getElementById("text-container").appendChild(textElement);
+    }
+    console.error(text);
+}
+
+function loadAudioFile(url, name) {
+    var request = new XMLHttpRequest();
+    request.open('GET', url, true);
+    request.responseType = 'arraybuffer';
+
+    request.onload = function() {
+        audioContext.decodeAudioData(request.response,
+            function(buffer) {
+                // Store the decoded audio buffer
+                audioBuffers[name] = buffer;
+            },
+            function() {
+                logError('Decoding error for audio file: ' + url);
+            }
+        );
+    };
+    request.onerror = function() {
+        logError('Network error while loading audio file: ' + url);
+    };
+
+    request.send();
+}
+
+function playAudio(name) {
+    // Get the audio buffer
+    var buffer = audioBuffers[name];
+    if (!buffer) {
+        logError('Audio file not loaded: ' + name);
+        return undefined;
+    }
+    logText("Playing " + name);
+    lastPlayedEnded = false;
+
+    // Create an audio source and connect it to the destination
+    var source = audioContext.createBufferSource();
+    source.buffer = buffer;
+    source.connect(audioContext.destination);
+
+    // Play the audio
+    source.start();
     
-    if(initialized == false) {
-	    console.log("Initializing...");
+    return source;
+}
+
+function clamp(num, min, max) {
+    return Math.min(Math.max(num, min), max);
+}
+
+function nextChannel(channel) {
+    return (channel + 1) % numChannels;
+}
+
+// ### STATE MACHINE ###
+
+class State {
+    constructor(app) {
+        this.app = app;
+    }
+
+    transition(newState) {
+        logText("Transitioning from " + this.app.currentState.name() + " to " + newState.name());
+        this.app.currentState = newState;
+    }
+}
+
+class OnIntroWithStationsUninitializedState extends State {
+    name() {
+        return "OnIntroWithStationsUninitializedState";
+    }
+    
+    startDisco() {
+        if(this.app.currentState != this) return;    // Handle race conditions
 	    
-	    let pageContainer = document.getElementById("page-container");
-	    
-	    // Create variables
-	    for (let i = 0; i < numChannels; i++) {
-	        let station = stations[i];
-	        station["fade"] = 0;
-	        
-	        // Create page
+        // User interaction is required to start the AudioContext due to autoplay policies in some browsers
+        if (audioContext.state === 'suspended') {
+            audioContext.resume();
+        }
+        for (let i = 0; i < numChannels; i++) {
+            stations[i]["audio"].muted = true;  // iOS doesn't seem to respect the takeover of the audio element... I have no idea why.
+            stations[i]["audio"].play();
+        }
+
+        // Push a new state to the history
+        history.pushState({ page: "disco" }, "", window.location.pathname);
+        
+        // Hide intro
+        document.getElementById('landing-page').style.display = "none";
+        
+        this.transition(new PlayingChannelNameState(this.app, 0));
+    }
+}
+
+class OnIntroWithStationsInitializedState extends State {
+    constructor(app) {
+        super(app);
+        
+        document.getElementById('landing-page').style.display = "block";
+    }
+    
+    name() {
+        return "OnIntroWithStationsInitializedState";
+    }
+
+    startDisco() {
+        if(this.app.currentState != this) return;    // Handle race conditions
+        
+        document.getElementById('landing-page').style.display = "none";
+        
+        // Push a new state to the history
+        history.pushState({ page: "disco" }, "", window.location.pathname);
+        
+        this.transition(new PlayingChannelNameState(this.app, 0));
+    }
+}
+
+class PlayingChannelNameState extends State {
+    constructor(app, channel) {
+        super(app);
+        this.channel = channel;
+        
+        stations[this.channel]["page"].style.display = "flex";
+        
+        let ret = playAudio(stations[this.channel]["name_audio"]);
+        if(ret == undefined) {
+            // Yikes! Presumably the name audio hasn't loaded yet? Just go straight to the station.
+            this.transition(new PlayingChannelState(this.app, this.channel));
+        }
+        else {
+            this.source = ret;
+        
+            this.source.addEventListener("ended", () => {
+                this.finish();
+            });
+        }
+    }
+    
+    name() {
+        return "PlayingChannelNameState(" + this.channel + ")";
+    }
+
+    finish() {
+        if(this.app.currentState != this) return;    // Handle race conditions
+        
+        this.source.disconnect(audioContext.destination);
+        
+        this.transition(new PlayingChannelState(this.app, this.channel));
+    }
+    
+    changeChannel() {
+        if(this.app.currentState != this) return;    // Handle race conditions
+        
+        this.source.disconnect(audioContext.destination);
+        this.source.stop();
+        stations[this.channel]["page"].style.display = "none";
+        
+        this.transition(new PlayingChannelNameState(this.app, nextChannel(this.channel)));
+    }
+    
+    goToIntro() {
+        if(this.app.currentState != this) return;    // Handle race conditions
+        
+        this.source.disconnect(audioContext.destination);
+        this.source.stop();
+        
+        this.transition(new OnIntroWithStationsInitializedState(this.app));
+    }
+}
+
+class PlayingChannelState extends State {
+    constructor(app, channel) {
+        super(app);
+        this.channel = channel;
+        
+        stations[this.channel]["source"].connect(audioContext.destination);
+        stations[this.channel]["audio"].muted = false;  // iOS doesn't seem to respect the takeover of the audio element... I have no idea why.
+    }
+    
+    name() {
+        return "PlayingChannelState(" + this.channel + ")";
+    }
+
+    changeChannel() {
+        if(this.app.currentState != this) return;    // Handle race conditions
+        
+        stations[this.channel]["source"].disconnect(audioContext.destination);
+        stations[this.channel]["audio"].muted = true;  // iOS doesn't seem to respect the takeover of the audio element... I have no idea why.
+        stations[this.channel]["page"].style.display = "none";
+        
+        this.transition(new PlayingChannelNameState(this.app, nextChannel(this.channel)));
+    }
+    
+    goToIntro() {
+        if(this.app.currentState != this) return;    // Handle race conditions
+        
+        stations[this.channel]["source"].disconnect(audioContext.destination);
+        stations[this.channel]["audio"].muted = true;  // iOS doesn't seem to respect the takeover of the audio element... I have no idea why.
+        stations[this.channel]["page"].style.display = "none";
+        
+        this.transition(new OnIntroWithStationsInitializedState(this.app));
+    }
+}
+
+class App {
+    constructor() {
+        logText("Initializing...");
+        
+        // Handle start button
+        document.getElementById("start-button").addEventListener('click', () => { this.currentState.startDisco(); });
+
+        // Handle back button
+        window.addEventListener("popstate", () => { this.currentState.goToIntro(); });
+        
+        // Create elements
+        let pageContainer = document.getElementById("page-container");
+        
+        // Create variables
+        for (let i = 0; i < numChannels; i++) {
+            let station = stations[i];
+            station["fade"] = 0;
+            
+            // Create page
             let page = document.createElement('div');
+            pageContainer.appendChild(page);
             page.style.backgroundColor = station["background-color"];
+            page.style.display = "none";
+            page.addEventListener('click', () => { this.currentState.changeChannel(); });
             station["page"] = page;
             page.classList.add('page');
             
@@ -132,106 +344,34 @@ function startDisco() {
             page.appendChild(img);
             
             // Create player
-            var playerAudio = document.createElement('audio');
-            station["player"] = playerAudio;
-		    playerAudio.setAttribute('src', station["stream"]);
-		    playerAudio.setAttribute('controls', '');
-		    playerAudio.setAttribute('autoplay', '');
-		    playerAudio.style.display = 'none';  // This hides the audio player
-		    playerAudio.volume = 0;
-		    page.appendChild(playerAudio);
-		    
-		    // Add page
-		    page.style.display = "none";    // Hide initially
-            pageContainer.appendChild(page);
+            var audioElement = new Audio();
+            station["audio"] = audioElement;
+            audioElement.crossOrigin = "anonymous";
+            audioElement.src = station["stream"];
+            var source = audioContext.createMediaElementSource(audioElement);
+            station["source"] = source;
+            
+            // Load the station name
+            loadAudioFile("audio/" + station["name_audio"], station["name_audio"]);
         }
-    
-	    // Add an event listener to the body that responds to click events
-	    document.body.addEventListener('click', changeChannel);
-    
-	    // Start the fade system
-	    setInterval(handleFade, fadeInterval);
-	    
-	    initialized = true;
+        
+        this.currentState = new OnIntroWithStationsUninitializedState(this);
     }
-    
-    // Set us to the first channel
-    currentChannel = 0;
-    playCurrentChannelName();
-
-    // Push a new state to the history
-    history.pushState({ page: "disco" }, "", window.location.pathname);
 }
 
-// When the history changes...
-window.addEventListener("popstate", function(event) {
-    // For now assume we're going back to the main state.
-    currentChannel = undefined;
-});
+let app = new App();
 
-const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
+// ### WAKE LOCK STUFF ###
 
-function playCurrentChannelName() {
-    // If a sound is playing, stop it
-    if (!audio.paused) {
-        audio.pause();
-        // To ensure the sound starts from the beginning when played again
-        audio.currentTime = 0;
-    }
-
-    // Load the next sound
-    audio.src = "audio/" + stations[currentChannel]["audio"];
-    console.log("Playing " + audio.src);
-    audio.play();
-}
-
-function handleFade() {
-    let status = '';
-   
-    // Handle intro page specially
-    if(currentChannel == undefined)
-        introFade = Math.min(introFade + fadeStep, 1.0);
-    else
-        introFade = Math.max(introFade - fadeStep, 0);
-    document.getElementById('landing-page').style.opacity = introFade;
-    document.getElementById('landing-page').style.display = (introFade > 0) ? "block" : "none";
-
-    // Now the station pages   
-    for (let i = 0; i < numChannels; i++) {
-        let station = stations[i];
-        if(i == currentChannel)
-            station["fade"] = Math.min(station["fade"] + fadeStep, 1.0);
-        else
-            station["fade"] = Math.max(station["fade"] - fadeStep, 0);
-        station["page"].style.opacity = station["fade"]
-        station["page"].style.display = (station["fade"] > 0) ? "flex" : "none";
-        station["player"].volume = clamp((station["fade"] - 0.8) * 2.5, 0.0, 0.5);
-        //status += i + ": " + station["fade"] + " ~ " + station["player"].volume + "   "
-    }
-    //console.log(status);
-}
-
-function changeChannel() {
-    if(introFade > 0) {
-        console.log("Click while still fading into the player; ignoring.");
-        return;
-    }
-
-    currentChannel = ((currentChannel + 1) % numChannels);
-    playCurrentChannelName();
-    console.log("Changed channel to " + currentChannel);
-}
-
-// Wake lock stuff
 let wakeLock = null;
 
 // Function that attempts to request a screen wake lock
 const requestWakeLock = async () => {
   try {
     wakeLock = await navigator.wakeLock.request('screen');
-    console.log('Screen Wake Lock is active');
+    logText('Screen Wake Lock is active');
   } catch (err) {
-    console.error(`Could not establish a wake lock (${err.name}: ${err.message})`);
+    logError(`Could not establish a wake lock (${err.name}: ${err.message})`);
   }
 }
 
@@ -240,11 +380,11 @@ const releaseWakeLock = () => {
   if (wakeLock !== null) {
     wakeLock.release()
     .then(() => {
-      console.log('Screen Wake Lock was released');
+      logText('Screen Wake Lock was released');
       wakeLock = null;
     })
     .catch((err) => {
-      console.error(`An error occurred while releasing the wake lock (${err.name}: ${err.message})`);
+      logError(`An error occurred while releasing the wake lock (${err.name}: ${err.message})`);
     });
   }
 }
@@ -255,8 +395,7 @@ window.addEventListener('load', () => {
     requestWakeLock();
   }
   else {
-    console.log("Wake lock not supported :(");
-    console.log(JSON.stringify(navigator));
+    logError("Wake lock not supported :( Navigator: " + JSON.stringify(navigator));
   }
 });
 
